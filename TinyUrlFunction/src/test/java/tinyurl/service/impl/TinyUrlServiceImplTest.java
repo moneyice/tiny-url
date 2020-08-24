708776d85810fcdf4eae5cce454f6d7835c2f43a
bing.a.qian <bing.a.qian@acc>
2020-08-24 11:38:32 +0800

    add region

--- TinyUrlFunction/src/test/java/tinyurl/service/impl/TinyUrlServiceImplTest.java
+++ TinyUrlFunction/src/test/java/tinyurl/service/impl/TinyUrlServiceImplTest.java
@@ -1,14 +1,16 @@
 package tinyurl.service.impl;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.IDynamoDBMapper;
-import tinyurl.BusinessException;
-import tinyurl.Constant;
-import tinyurl.entity.TinyUrl;
-import tinyurl.repository.TinyUrlRepository;
-import tinyurl.service.TinyUrlService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import tinyurl.BusinessException;
+import tinyurl.entity.TinyUrl;
+import tinyurl.repository.TinyUrlRepository;
+import tinyurl.service.TinyUrlService;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -33,9 +35,18 @@ public class TinyUrlServiceImplTest {
     @Test
     public void shortenLongUrl() throws BusinessException {
         String longUrl = "http://www.bing.com";
+        //执行
         String shortUrl = tinyUrlService.shortenLongUrl(longUrl);
+        //验证
+        assertEquals("ok", 10, shortUrl.length());
+        String pipeRegex ="^[a-zA-Z0-9]+$";
+        Pattern pipePattern = Pattern.compile(pipeRegex, Pattern.CASE_INSENSITIVE);
 
+        Matcher pipeMatcher = pipePattern.matcher(shortUrl);
+
+        assertEquals("all charactors", true,pipeMatcher.matches());
         verify(dynamoDBMapper, times(1)).save(any());
+
 
     }
 
@@ -47,11 +58,14 @@ public class TinyUrlServiceImplTest {
         TinyUrl returnTinyUrl = new TinyUrl();
         returnTinyUrl.setLongUrl(longUrl);
 
+        //假设
         when(dynamoDBMapper.load(TinyUrl.class, 315151157690118144L)).thenReturn(returnTinyUrl);
 
+        //执行
         String shortUrl = "NHOWrSHp6e";
         String resultLongUrl = tinyUrlService.getLongUrl(shortUrl);
 
+        //验证
         verify(dynamoDBMapper, times(1)).load(any(), eq(315151157690118144L));
         assertEquals("getLongUrl", longUrl, resultLongUrl);
     }
@@ -61,10 +75,11 @@ public class TinyUrlServiceImplTest {
         //if url not found, return Constant.DEFAULT_REDIRECT_URL
 
         //  base62(315151157690118144)= NHOWrSHp6e
-
+        //假设
         //not found the tiny url
         when(dynamoDBMapper.load(TinyUrl.class, 315151157690118144L)).thenReturn(null);
 
+        //执行
         String shortUrl = "NHOWrSHp6e";
         try {
             String resultLongUrl = tinyUrlService.getLongUrl(shortUrl);
@@ -72,6 +87,7 @@ public class TinyUrlServiceImplTest {
         }catch (BusinessException e){
             assertEquals("tiny url is invalid","tiny url is invalid",e.getMessage());
         }
+        //验证
         verify(dynamoDBMapper, times(1)).load(any(), eq(315151157690118144L));
     }
 
@@ -83,17 +99,20 @@ public class TinyUrlServiceImplTest {
         TinyUrl returnTinyUrl = new TinyUrl();
         returnTinyUrl.setLongUrl("");
 
+        //假设
         //not found the tiny url
         when(dynamoDBMapper.load(TinyUrl.class, 315151157690118144L)).thenReturn(returnTinyUrl);
 
         String shortUrl = "NHOWrSHp6e";
 
+        //执行
         try {
             String resultLongUrl = tinyUrlService.getLongUrl(shortUrl);
             fail();
         }catch (BusinessException e){
             assertEquals("tiny url is invalid","tiny url is invalid",e.getMessage());
         }
+        //验证
         verify(dynamoDBMapper, times(1)).load(any(), eq(315151157690118144L));
     }
 }
